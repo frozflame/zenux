a6235d10eeafbb26bcf2741d2c22bee6381c8d8e
Hailong <lendoli@163.com>
2020-01-14 21:03:21 +0800

    bodyToDiv, urlToElement, pageToElements

--- zenux-dom/dom.js
+++ zenux-dom/dom.js
@@ -1,13 +1,28 @@
 /* globals exports */
+/* globals require */
 /* jshint node: true */
 /* jshint eqnull: true */
 
 /* jshint curly: false */
 
+var $ = require('jquery');
+
+
+function bodyToDiv(html){
+    var bodyContent = /<body[^>]*>(.*?)<\/body>/ims.exec(html)[1];
+    var div = document.createElement('div');
+    div.innerHTML = bodyContent;
+    return div
+}
+
+
 function HTMLEscaper() {
     var that = this;
     that.textarea = document.createElement('textarea');
     that.escape = function (html) {
+        if (typeof html !== "string") {
+            html = html.outerHTML;
+        }
         that.textarea.textContent = html;
         return that.textarea.innerHTML;
     };
@@ -26,24 +41,38 @@ function createElement(name, attrs, children) {
     children.forEach(function (child) {
         elem.appendChild(child);
     });
+    return elem;
 }
 
-function anchorToElement(anchor) {
-    if (/.*\.js$/.test(anchor.pathname)) {
-        return createElement('script', {src: anchor.href, type: 'text/javascript'}, []);
+
+function urlToElement(url, urlbase) {
+    url = new URL(url, urlbase || window.location.href);
+    if (/.*\.js$/.test(url.pathname)) {
+        return createElement('script', {src: url.href, type: 'text/javascript'}, []);
     }
-    if (/.*\.css$/.test(anchor.pathname)) {
-        return createElement('link', {href: anchor.href, rel: 'stylesheet', type: 'text/css'}, []);
+    if (/.*\.css$/.test(url.pathname)) {
+        return createElement('link', {href: url.href, rel: 'stylesheet', type: 'text/css'}, []);
     }
+    if (/.*\.(jpe?g|png|bmp|gif|tiff?)$/.test(url.pathname)) {
+        return createElement('img', {src: url.href}, []);
+    }
+    return createElement('a', {href: url.href}, []);
 }
 
-function urlToElement(url) {
-    var anchor = document.createElement('a');
-    anchor.href = url;
-    return anchorToElement(anchor);
+
+function pageToElements(html, urlbase, selector, attrname) {
+    var anchors = $(html).find(selector || 'a').toArray();
+    return anchors.map(function (anc) {
+        // Notice: anchor.href is different from anchor.getAttribute('href')
+        return zenux_dom.urlToElement(
+            anc.getAttribute(attrname || 'href'), urlbase);
+    });
 }
 
 
+exports.bodyToDiv = bodyToDiv;
 exports.HTMLEscaper = HTMLEscaper;
-exports.anchorToElement = anchorToElement;
+exports.createElement = createElement;
 exports.urlToElement = urlToElement;
+exports.pageToElements = pageToElements;
+exports.$ = $;
