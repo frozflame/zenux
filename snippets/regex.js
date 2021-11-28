51076178ef8fe02a596b43a7fc54dc29e2f17c67
frozflame <frozflame@outlook.com>
2021-11-28 09:40:20 +0800

    remove zenux-* from zenux

--- snippets/regex.js
+++
@@ -1,6 +0,0 @@
-
-var regexes = [
-    // http url: char "'" is excluded
-    /https?:\/\/[-0-9A-Z._~:/?#\[\]@!$&()*+,;=]+/gi,
-];
-
--- snippets/scroll.js
+++
@@ -1,9 +0,0 @@
-function scroll(){
-    "use strict";
-    setInterval(1000, function () {
-        window.scrollBy(0, document.body.scrollHeight);
-    });
-}
-
-scroll();
-
